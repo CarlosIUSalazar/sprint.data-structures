6f789c6493b2ce9dde2a4f8ab945c871e77dc914
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-04-10 17:04:25 +0900

    Current version

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,10 +1,10 @@
-// class Node {
-//   constructor(value) {
-//     this.value = value;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
 class BinarySearchTree {
   constructor() {
@@ -18,37 +18,37 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  //   insert(value) {
-  //     let newNode = new this.newNode(value);
-  //     if (this.root === null) {
-  //       //if there is no root
-  //       this.root = newNode;
-  //       return this;
-  //     } else {
-  //       //if there is a root then
-  //       let current = this.root;
-  //       while (true) {
-  //         if (value === current.value) return undefined; //for duplicates
-  //         if (value < current.value) {
-  //           if (current.left === null) {
-  //             //if there is nothing on the left we found our spot
-  //             current.left = newNode;
-  //             return this;
-  //           } else {
-  //             current = current.left;
-  //           }
-  //         } else if (value > current.value) {
-  //           if (current.right === null) {
-  //             current.right = newNode;
-  //             return this;
-  //           } else {
-  //             //keep looking
-  //             current = current.right;
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
+  insert(value) {
+    let newNode = new this.newNode(value);
+    if (this.root === null) {
+      //if there is no root
+      this.root = newNode;
+      return this;
+    } else {
+      //if there is a root then
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined; //for duplicates
+        if (value < current.value) {
+          if (current.left === null) {
+            //if there is nothing on the left we found our spot
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            //keep looking
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 
   contains(value) {
     if (this.root === null) {
@@ -84,4 +84,4 @@ class BinarySearchTree {
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 */
 
-module.exports = { BinarySearchTree };
+module.exports = BinarySearchTree;
--- test/_tree.js
+++ test/_tree.js
@@ -61,7 +61,7 @@ describe("Trees", () => {
     });
   });
 
-  describe.only("The remove method", () => {
+  describe("The remove method", () => {
     it("should exist on the Tree prototype", () => {
       expect(Tree.prototype.remove).to.exist;
     });
